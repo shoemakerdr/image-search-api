39e4f6c18141b29fafe68c50ec5e97f2e026d309
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-25 21:36:56 +0000

    Added @std/esm invocation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-const path = require('path')
-const express = require('express')
-const shortener = require('./shortener')
-
-const app = express()
-
-const publicPath = path.join(__dirname, '../', 'public')
-const html = path.normalize(path.join(publicPath, 'index.html'))
-
-app.use('/', express.static(path.normalize(publicPath)))
-
-app.get('/', (req,res) => {
-    res.sendFile(html)
-})
-
-app.get('/:resource', (req, res) => {
-    const resource = req.params.resource
-    shortener(resource, res)
-})
-
-app.listen(process.env.PORT || 8000)
+// enable @std/esm loader
+require = require('@std/esm')(module, { cjs: true, esm: "js" })
+module.exports = require('./main.js').default
