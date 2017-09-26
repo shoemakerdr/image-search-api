3820b9e5a23df4a3bdedf74e1f6d3a2bd3f57d33
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-25 21:47:47 -0500

    Finished db functions

--- src/db.js
+++ src/db.js
@@ -8,33 +8,31 @@ const dbError = {
             }
         }
 
-const getLatest = handler => {
-    handler(dbError)
-    // MongoClient.connect(databaseURL, (err, db) => {
-    //     if (err) {
-    //         console.error(err)
-    //         handler(dbError)
-    //     }
-        
-    //     console.log('MongoClient connected.')
-        
-    //     const latest = db.collection('latest')
-        
-    //     latest.updateOne().then(handler).catch(() => handler(dbError))
-
-    // })
+const getLatest = responseObj => {
+    MongoClient.connect(databaseURL, (err, db) => {
+        if (err) responseObj.json(dbError)
+        const images = db.collection('images')
+        images.find({}, {fields: {_id: 0}})
+            .sort({when: -1})
+            .toArray()
+            .then(array => array.slice(0,10))
+            .then(res => responseObj.json(res))
+            .then(() => db.close)
+            .catch(() => responseObj.json(dbError))
+    })
 }
 
 const updateLatest = (term, timestamp) => {
+    const doc = {term: term, when: timestamp}
     MongoClient.connect(databaseURL, (err, db) => {
-        if (err) return console.error(err)
-        
-        console.log('MongoClient connected.')
-        
-        // FORMAT: {"term":"lolcats funny","when":"2017-09-25T20:20:15.226Z"}
-        
-        const latest = db.collection('latest')
-
+        if (err) console.error(err)
+        const images = db.collection('images')
+        images.insert(doc)
+            .then(() => {
+                images.findOneAndDelete({}, {sort: {when: 1}})
+                    .then(() => db.close)
+                    .catch(console.error)
+        }).catch(console.error)
     })
 }
 
