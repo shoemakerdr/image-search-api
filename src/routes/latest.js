af0f7bc4e41ceb9b798691493577b58c02f6be6f
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-26 14:43:11 +0000

    Explicitly binding res to .json method and passing that into getLatest.

--- src/routes/latest.js
+++ src/routes/latest.js
@@ -4,7 +4,10 @@ import { getLatest } from '../db'
 const router = Router()
 
 router.get('/', (req, res) => {
-    getLatest(res)
+    // Must explicitly bind res object to json method because passing res.json 
+    // to getLatest will cause .json method to lose `this` binding.
+    const json = res.json.bind(res)
+    getLatest(json)
 })
 
 export default router
