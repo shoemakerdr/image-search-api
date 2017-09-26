497d461334654842a3fff99b2d7f0181c6d1f0cb
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-26 14:44:47 +0000

    Deleted trailing whitespace

--- src/routes/latest.js
+++ src/routes/latest.js
@@ -4,7 +4,7 @@ import { getLatest } from '../db'
 const router = Router()
 
 router.get('/', (req, res) => {
-    // Must explicitly bind res object to json method because passing res.json 
+    // Must explicitly bind res object to json method because passing res.json
     // to getLatest will cause .json method to lose `this` binding.
     const json = res.json.bind(res)
     getLatest(json)
