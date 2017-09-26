407b8170a60a8c5bab642289da9d4c607a45b398
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-26 14:42:12 +0000

    Changed getLatest argument to take a simple callback instead of response object. Changed updateLatest to make one findOneAndReplace instead of two separate insert and findOneAndDelete calls

--- src/db.js
+++ src/db.js
@@ -2,23 +2,23 @@ import { MongoClient } from 'mongodb'
 const databaseURL = process.env.MLAB_URI
 
 const dbError = {
-            error: {
-                code: 503,
-                message: 'There was an issue with the database.'
-            }
-        }
+    error: {
+        code: 503,
+        message: 'There was an issue with the database.'
+    }
+}
 
-const getLatest = responseObj => {
+const getLatest = handler => {
     MongoClient.connect(databaseURL, (err, db) => {
-        if (err) responseObj.json(dbError)
+        if (err) handler(dbError)
         const images = db.collection('images')
         images.find({}, {fields: {_id: 0}})
             .sort({when: -1})
             .toArray()
             .then(array => array.slice(0,10))
-            .then(res => responseObj.json(res))
+            .then(res => handler(res))
             .then(() => db.close)
-            .catch(() => responseObj.json(dbError))
+            .catch(() => handler(dbError))
     })
 }
 
@@ -27,12 +27,9 @@ const updateLatest = (term, timestamp) => {
     MongoClient.connect(databaseURL, (err, db) => {
         if (err) console.error(err)
         const images = db.collection('images')
-        images.insert(doc)
-            .then(() => {
-                images.findOneAndDelete({}, {sort: {when: 1}})
-                    .then(() => db.close)
-                    .catch(console.error)
-        }).catch(console.error)
+        images.findOneAndReplace({}, doc, {sort: {when: 1}})
+            .then(() => db.close)
+            .catch(console.error)
     })
 }
 
