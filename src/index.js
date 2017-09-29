11458576721088ceeeb56a40d6153b648484ba54
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-29 00:09:08 -0500

    Added try/catch around dotenv require.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // for .env vars
-require('dotenv').config()
+try {
+    require('dotenv').config()
+} catch (err) {
+    console.log(err)
+}
 // enable @std/esm loader
 require = require('@std/esm')(module, { cjs: true, esm: "js" })
 module.exports = require('./main.js').default
